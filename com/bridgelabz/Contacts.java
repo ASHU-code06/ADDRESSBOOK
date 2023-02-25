22277279bc831c4fa0f8440a74942e614d9878db
ASHU-code06 <ashuc306@gmail.com>
2023-02-25 17:07:03 +0530

    Added editContact Method

--- com/bridgelabz/Contacts.java
+++ com/bridgelabz/Contacts.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import java.util.Scanner;
   class Contacts {
         public int contactId;
-       ArrayList<ContactModel> contactlist = new ArrayList<ContactModel>();
+       ArrayList<ContactModel> contactList = new ArrayList<ContactModel>();
        public void addContact(){
            //Method for creating contacts only
            ContactModel contactModel = new ContactModel();//creating object of other class
@@ -30,6 +30,98 @@ import java.util.Scanner;
            contactModel.email = input.nextLine();
            System.out.println("Your ContactId is "+contactModel.contactId);
 
-           contactlist.add(contactModel);//Added contact details in contactModel
+           contactList.add(contactModel);//Added contact details in contactModel
        }
-}
+       void displayContact(){
+           for (int i=0;i<contactList.size();i++){
+                ContactModel obj = contactList.get(i);
+               System.out.println("Name : "+obj.firstName+" "+obj.lastName);
+               System.out.println("Address : "+obj.address);
+               System.out.println("City : "+obj.city);
+               System.out.println("State : "+obj.state);
+               System.out.println("zip : "+obj.zip);
+               System.out.println("Phone-Number : "+obj.phoneNumber);
+               System.out.println("Email : "+obj.email);
+           }
+       }
+      private int getContactIndex(String name) {
+          for (int i = 0; i < contactList.size(); i++) {
+              if (contactList.get(i).firstName.equals(name)) {
+                  return i;
+              }
+          }
+          return -1;
+      }
+
+      public void editContact(String name) {
+          int contactIndex = getContactIndex(name);
+          if (contactIndex == -1) {
+              System.out.println("Contact Not Exists!");
+              return;
+          }
+
+
+          boolean isEdit = true;
+          while (isEdit) {
+              System.out.println("SELECT ");
+              System.out.println(
+                      "1. firstname \n" +
+                              "2. lastname \n" +
+                              "3. address \n" +
+                              "4. city \n" +
+                              "5. state \n" +
+                              "6. zip \n" +
+                              "7. phone-number \n" +
+                              "8. email \n" +
+                              "9. exit ");
+              int choice = (new Scanner(System.in)).nextInt();
+              switch (choice) {
+                  case 1:
+                      System.out.println("Enter First Name: ");
+                      String firstName = (new Scanner(System.in)).nextLine();
+                      contactList.get(contactIndex).firstName = firstName;
+                      break;
+                  case 2:
+                      System.out.println("Enter Last Name: ");
+                      String lastName = (new Scanner(System.in)).nextLine();
+                      contactList.get(contactIndex).lastName = lastName;
+                      break;
+                  case 3:
+                      System.out.println("Enter Address: ");
+                      String address = (new Scanner(System.in)).nextLine();
+                      contactList.get(contactIndex).address = address;
+                      break;
+                  case 4:
+                      System.out.println("Enter City: ");
+                      String city = (new Scanner(System.in)).nextLine();
+                      contactList.get(contactIndex).city = city;
+                      break;
+                  case 5:
+                      System.out.println("Enter State: ");
+                      String state = (new Scanner(System.in)).nextLine();
+                      contactList.get(contactIndex).state = state;
+                      break;
+                  case 6:
+                      System.out.println("Enter Zip: ");
+                      String zip = (new Scanner(System.in)).nextLine();
+                      contactList.get(contactIndex).zip = zip;
+                      break;
+                  case 7:
+                      System.out.println("Enter PhoneNo: ");
+                      String phoneNumber = (new Scanner(System.in)).nextLine();
+                      contactList.get(contactIndex).phoneNumber = phoneNumber;
+                      break;
+                  case 8:
+                      System.out.println("Enter Email Id: ");
+                      String email = (new Scanner(System.in)).nextLine();
+                      contactList.get(contactIndex).email = email;
+                      break;
+                  case 9:
+                      isEdit = false;
+                      break;
+                  default:
+                      System.out.println("Invalid Option!, try Again");
+              }   break;
+          }
+      }
+  }
--- com/bridgelabz/Main.java
+++ com/bridgelabz/Main.java
@@ -3,6 +3,7 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class Main {
+    Scanner objec = new Scanner(System.in);
     Contacts contacts = new Contacts();//created object of class Contacts
     public static void main(String args[]){
     Main main = new Main();//created object of Main class to call non-static method  of this class
@@ -11,15 +12,23 @@ public class Main {
     private void initiate(){
         System.out.println("Press 1 for add contact");
         System.out.println("Press 2 to display contact");
-        int input = new Scanner(System.in).nextInt();
+        System.out.println("Press 3 to edit contact");
+        int input = objec.nextInt();
         switch (input){
             case 1:
                 contacts.addContact();
                 System.out.println("CONTACT ADDED !");
+                initiate();
                 break;
             case 2:
                 contacts.displayContact();
                 break;
+            case 3:
+                System.out.println("Enter name for editing the contact");
+                String nameForEditContactDetails = new Scanner(System.in).nextLine();
+                contacts.editContact(nameForEditContactDetails);
+                System.out.println("Contact edited successfully");
+                initiate();
             default:
                 System.out.println("Invalid Input");
         }
