ac18d158ac8c4de7290c753ffbd3e3c7f4f4a191
ASHU-code06 <ashuc306@gmail.com>
2023-04-07 01:05:27 +0530

    used hash map to add multiple address books

--- com/bridgelabz/Main.java
+++ com/bridgelabz/Main.java
@@ -1,12 +1,14 @@
 package com.bridgelabz;
 
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+    String name;
     Scanner object = new Scanner(System.in);
     AddressBook addressBook = new AddressBook();//created object of class Contacts
-    ArrayList<String> AddressBook = new ArrayList<>();
+   // ArrayList<String> AddressBook = new ArrayList<>();
     public static void main(String args[]){
     Main main = new Main();//created object of Main class to call non-static method  of this class
     main.initiate();
@@ -23,27 +25,28 @@ public class Main {
         switch (input){
             case 1:
                 addressBook.addContact();
-                System.out.println("CONTACT ADDED !");
+                System.out.println("SUCCESSFULLY ADDED !");
                 initiate();
                 break;
             case 2:
-                System.out.println("AddressBook has following contacts");
-                addressBook.displayContact();
+                System.out.println("*****************************************************************************************");
+                //addressBook.displayContact();
+                addressBook.displayAddressBooks();
                 initiate();
                 break;
-            case 3:
-                System.out.println("Enter name for editing the contact");
-                String nameForEditContactDetails = new Scanner(System.in).nextLine();
-                addressBook.editContact(nameForEditContactDetails);
-                System.out.println("Contact edited successfully");
-                initiate();
-            case 4:
-                System.out.println("Enter Name for Delete Contact");
-                String contactNameForDelete = (new Scanner(System.in)).nextLine();
-                addressBook.deleteContact(contactNameForDelete);
-                System.out.println("Deleted successfully");
-                initiate();
-                break;
+//            case 3:
+////                System.out.println("Enter name for editing the contact");
+////                String nameForEditContactDetails = new Scanner(System.in).nextLine();
+////                addressBook.editContact(nameForEditContactDetails);
+////                System.out.println("Contact edited successfully");
+////                initiate();
+//            case 4:
+////                System.out.println("Enter Name for Delete Contact");
+////                String contactNameForDelete = (new Scanner(System.in)).nextLine();
+////                addressBook.deleteContact(contactNameForDelete);
+////                System.out.println("Deleted successfully");
+////                initiate();
+////                break;
             case 5:
                 System.out.println("Exit successfully");
                 break;
